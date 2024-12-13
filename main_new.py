25ed718ac4491d1b2137462f8e6ad07fd65a1bd6
Erick Cruz <erickvrc@outlook.com>
2024-12-13 12:17:10 -0300

    Delete main_new.py

--- main_new.py
+++
@@ -1,9 +0,0 @@
-from deepface import DeepFace
-import pandas as pd
-import cv2
-
-
-image_path = 'image2.jpg'
-image = cv2.imread(image_path)
-
-df = DeepFace.represent(image, model_name="Facenet")[0]
